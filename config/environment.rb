02dc4cc5d83a89b463ab9a6def7a507b8b056132
aidanmc95 <aidanmc95@gmail.com>
2020-05-06 11:48:40 -0700

    Change budget, delete user, create user

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,7 @@ ActiveRecord::Base.establish_connection(
     adapter: 'sqlite3', 
     database: 'db/development.db'
 )
+ActiveRecord::Base.logger = nil
+
 require_all 'lib'
 require_all 'app/models'
