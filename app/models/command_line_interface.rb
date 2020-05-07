27b39bac68564b79455e6759dbe100bc4dc94e8c
johnrusch <johnrusch7@gmail.com>
2020-05-07 10:55:09 -0700

    first day 4 morning commit

--- app/models/command_line_interface.rb
+++ app/models/command_line_interface.rb
@@ -4,124 +4,201 @@ class CommandLineInterface
     def initialize
     end
 
-    def space
-        puts "\n"
-        puts "\n"
-        puts "\n"
-    end
-
-    # Greets the user upon opening MovieBook
-    def greet
-        self.space
-        puts "*********************"
-        puts 'Welcome to MovieBook!'
-        puts "*********************"
-        self.space
-    end
 
 
-    def user_account(answer)
-        # self.find_or_create_user_account(answer)
-        User.find_or_create_by(name: answer)
-    end
-
-    def find_movie(answer)
-        Movie.search_movie_database(answer)
-    end
-
-   
-
-    def write_review(write_up, rating)
-        review = Review.create(user_id: @user[:id], movie_id: @movie[:id], write_up: write_up, rating: rating)
-        puts "#{@movie[:title]} --- #{rating}/5 --- #{write_up}"
-        @user.reviews << review
-    end
-
-    def movie_menu_read_reviews
-        self.space
-        puts @movie[:title]
-        puts "\n"
-        Review.where(movie_id: @movie[:id]).map do |review|
-            puts "#{review[:rating]} --- #{review[:write_up]}"
+    # Greeting functions
+        def greet
+            self.space
+            puts "*********************"
+            puts 'Welcome to MovieBook!'
+            puts "*********************"
+            self.space
         end
-    end
+        
+        def user_account(answer)
+            # self.find_or_create_user_account(answer)
+            User.find_or_create_by(name: answer)
+        end
 
 
 
+    # Movie functions
+        def find_movie(answer)
+            Movie.search_movie_database(answer)
+        end
 
-    def movie_menu_write_review(cli)
-        cli.space
-        puts "Write your review:"
-        write_up = gets.chomp
-        cli.space
-        puts "Now rate this movie from 1 to 5:"
-        rating = gets.chomp.to_i.clamp(1, 5)
-        cli.space
-        cli.write_review(write_up, rating)
-        main_menu(cli)
-    end
+        def movie_menu(cli)
+            #Prompts user to find movie in database
+            system("clear")
+            puts "What movie are you looking for?"
+            user_input = gets.strip
+            cli.movie = cli.find_movie(user_input)
+
+            #output movie info: title, desc
+            puts cli.movie[:title]
+
+            #give option to write/read review, or go back to main_menu
+            system("clear")
+            cli.movie_options
+            user_input = gets.chomp.to_i
+        end
+
+        def write_review(write_up, rating)
+            review = Review.create(user_id: @user[:id], movie_id: @movie[:id], write_up: write_up, rating: rating)
+            puts "Here is your review:"
+            puts "#{@movie[:title]} --- #{rating}/5 --- #{write_up}"
+            self.space
+            @user.reviews << review
+        end
+
+        def movie_menu_read_reviews
+            self.space
+            puts @movie[:title]
+            puts "\n"
+            Review.where(movie_id: @movie[:id]).map do |review|
+                puts "#{review[:rating]} --- #{review[:write_up]}"
+            end
+        end
+
+        def movie_menu_write_review(cli)
+            puts "Write your review:"
+            write_up = gets.chomp
+            system("clear")
+            puts "Now rate this movie from 1 to 5:"
+            rating = gets.chomp.to_i.clamp(1, 5)
+            system("clear")
+            cli.write_review(write_up, rating)
+            main_menu(cli)
+        end
+    
+    
+        
+        
+     # User Settings functions   
+        def update_account(user_name)
+            #Finds current instance of user
+            user = User.find_user(user_name)
+            self.space
+            #prompts user for new name and stores in variable
+            puts "Enter your new name:"
+            user_input = gets.strip
+    
+            #puts in new name and stores in current user instance
+            User.change_name(user, user_input)
+            self.space
+    
+            #tells user name change successful
+            puts "Your name has been changed to: #{user_input}"
+        end
+
+        def delete_account(cli)
+            user = User.find_user(cli.user.name)
+            user.destroy
+            puts "Account successfully deleted! Goodbye!"
+        end
+
+        def update_review(cli)
+            #finds a user's reviews
+            puts "Here are your reviews:"
+            puts "\n"
+            reviews = cli.user.find_reviews
+            # system("clear")
+
+            #prompts user to pick one of their reviews
+            puts "\n"
+            puts "Please select a review to update:"
+            user_input = gets.chomp.to_i
+            puts "\n"
+            #stores selected review in a var
+            selected_review = reviews[(user_input)-1]
+            system("clear")
+            #ask for new review and rating
+            puts "Write your new review:"
+            write_up = gets.chomp
+            system("clear")
+            puts "Now rate this movie from 1 to 5:"
+            rating = gets.chomp.to_i.clamp(1, 5)
+            Review.update_review(selected_review, rating, write_up)
+            system("clear")
+            puts "Here's your updated review!"
+            puts "\n"
+            puts "#{selected_review.movie.title} --- #{rating} --- #{write_up}"
+            cli.space
+        end
+
+        def delete_review(cli)
+            #finds a users reviews
+            puts "Here are your reviews:"
+            puts "\n"
+            reviews = cli.user.find_reviews
+            cli.space
+
+            #prompts user to pick one of their reviews
+            puts "Please select a review to delete:"
+            user_input = gets.chomp.to_i
+            cli.space
+
+            #stores selected review in a var
+            selected_review = reviews[(user_input)-1]
+                    
+            #deletes selected review
+            selected_review.destroy
+            puts "You have deleted your review!"
+            cli.space
+
+        end
+
 
    
 
     #options prompts
-    def main_menu_options
-        puts "Please select from the following"
-        puts "\n"
-        puts "1. Movies: info, reviews"
-        puts "\n"
-        puts "2. User Settings"
-        puts "\n"
-        puts "3. Exit"
-        puts "\n"
-    end
+        def main_menu_options
+            puts "Please select from the following"
+            puts "\n"
+            puts "1. Movies: info, reviews"
+            puts "\n"
+            puts "2. User Settings"
+            puts "\n"
+            puts "3. Exit"
+            puts "\n"
+        end
 
-    def movie_options
-        puts "\n"
-        puts "Please select from the following"
-        puts "\n"
-        puts "1. Write a review"
-        puts "\n"
-        puts "2. Read reviews"
-        puts "\n"
-        puts "3. Return to main menu"
-        puts "\n"
-    end
+        def movie_options
+            puts "\n"
+            puts "Please select from the following"
+            puts "\n"
+            puts "1. Write a review"
+            puts "\n"
+            puts "2. Read reviews"
+            puts "\n"
+            puts "3. Return to main menu"
+            puts "\n"
+        end
 
-    def user_settings_options
-        puts "Please select from the following"
-        puts "\n"
-        puts "1. Update account"
-        puts "\n"
-        puts "2. Delete account"
-        puts "\n"
-        puts "3. Update a review"
-        puts "\n"
-        puts "4. Delete a review"
-        puts "\n"
-        puts "5. Return to main menu"
-        puts "\n"
-    end
+        def user_settings_options
+            puts "Please select from the following"
+            puts "\n"
+            puts "1. Update account"
+            puts "\n"
+            puts "2. Delete account"
+            puts "\n"
+            puts "3. Update a review"
+            puts "\n"
+            puts "4. Delete a review"
+            puts "\n"
+            puts "5. Return to main menu"
+            puts "\n"
+        end
 
     
-    def update_account(user_name)
-        #Finds current instance of user
-        user = User.find_user(user_name)
-        self.space
-        #prompts user for new name and stores in variable
-        puts "Enter your new name:"
-        user_input = gets.strip
-
-        #puts in new name and stores in current user instance
-        User.change_name(user, user_input)
-        self.space
-
-        #tells user name change successful
-        puts "Your name has been changed to: #{user_input}"
-    end
 
 
- 
-
+    #Auxiliary functions
+        def space
+            puts "\n"
+            puts "\n"
+            puts "\n"
+        end
 
 
 
--- bin/run.rb
+++ bin/run.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 #creates new instance of CommandLineInterface
 cli = CommandLineInterface.new
+system("clear")
 
 #Welcomes user to MovieBook
 cli.greet
@@ -15,40 +16,29 @@ def account_prompt(cli)
     cli.user = cli.user_account(user_input)
 end
 
+
 #Gives user list of options
 def main_menu(cli)
-    cli.space 
     cli.main_menu_options
     user_input = gets.chomp.to_i
 
-    case user_input
+    case user_input #Main Menu
         when 1 #Movie Menu
-            #Prompts user to find movie in database
-            cli.space
-            puts "What movie are you looking for?"
-            user_input = gets.strip
-            cli.movie = cli.find_movie(user_input)
-
-            #output movie info: title, desc
-            puts cli.movie[:title]
-
-            #give option to write/read review, or go back to main_menu
-            cli.movie_options
-            user_input = gets.chomp.to_i
-
-            case user_input
+            cli.movie_menu(cli)
+            case user_input #Menu for selected movie
                 when 1 #write new review 
+                    system("clear")
                     cli.movie_menu_write_review(cli)
                 when 2 #read reviews 
+                    system("clear")
                     puts cli.movie_menu_read_reviews
                     main_menu(cli)
                 when 3 #exit to main menu
                     main_menu(cli)
             end
-        when 2
-            #option 2 - User Settings Menu
+        when 2 #User Settings Menu
             #update/delete account -or- logout, should send back to user account prompt
-            cli.space
+            system("clear")
             cli.user_settings_options
             user_input = gets.chomp.to_i
             case user_input
@@ -56,71 +46,32 @@ def main_menu(cli)
                     cli.update_account(cli.user.name)
                     main_menu(cli)
                 when 2 #delete account
+                    system("clear")
+                    cli.delete_account(cli)
                     cli.space
-                    user = User.find_user(cli.user.name)
-                    # binding.pry
-                    user.destroy
-                    puts "Account successfully deleted! Goodbye!"
-                    cli.space
-                    
                 when 3 #update review
-                    #finds a users reviews
-                    cli.space
-                    puts "Please select a review:"
-                    puts "\n"
-                    reviews = cli.user.find_reviews
-                    cli.space
-
-                    #prompts user to pick one of their reviews
-                    user_input = gets.chomp.to_i
-                    puts "\n"
-                    #stores selected review in a var
-                    selected_review = reviews[(user_input)-1]
-                    #ask for new review and rating
-                    puts "Write your review:"
-                    write_up = gets.chomp
-                    puts "\n"
-                    puts "Now rate this movie from 1 to 5:"
-                    rating = gets.chomp.to_i.clamp(1, 5)
-                    Review.update_review(selected_review, rating, write_up)
-                    cli.space
-                    puts "Here's your updated review!"
-                    puts "\n"
-                    puts "#{selected_review.movie.title} --- #{rating} --- #{write_up}"
+                    system("clear")
+                    cli.update_review(cli)
                     main_menu(cli)
-
                 when 4 #delete a review
                     #finds a users reviews
-                    cli.space
-                    reviews = cli.user.find_reviews
-                    cli.space
-
-                    #prompts user to pick one of their reviews
-                    puts "Please select a review"
-                    user_input = gets.chomp.to_i
-                    cli.space
-
-                    #stores selected review in a var
-                    selected_review = reviews[(user_input)-1]
-                    
-                    #deletes selected review
-                    selected_review.destroy
-                    puts "You have deleted your review!"
-
+                    system("clear")
+                    cli.delete_review(cli)
                     main_menu(cli)
                 when 5 #return to main menu
-                    
                     main_menu(cli)
             end
 
-        when 3 #exit main_menu function/program
-            cli.space
+        when 3 #Exit
+            system("clear")
             puts "Thanks for using MovieBook! Goodbye!"
             cli.space
+            
     end
 end
 
 account_prompt(cli)
+system("clear")
 main_menu(cli)
 
 # binding.pry
